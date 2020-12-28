1926e00ffbdf7ef5270ed73099c73d527976671f
Maschell <Maschell@gmx.de>
2020-12-28 14:45:41 +0100

    Add FunctionPatcherRestoreDynamicFunctions

--- include/function_patcher/function_patching.h
+++ include/function_patcher/function_patching.h
@@ -7,8 +7,12 @@
 extern "C" {
 #endif
 
+extern void FunctionPatcherRestoreDynamicFunctions(function_replacement_data_t *replacements, uint32_t size);
+
 extern void FunctionPatcherPatchFunction(function_replacement_data_t *replacements, uint32_t size);
+
 extern void FunctionPatcherRestoreFunctions(function_replacement_data_t *replacements, uint32_t size);
+
 #ifdef __cplusplus
 }
 #endif
