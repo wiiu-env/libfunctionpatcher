5173f8609cdbd42c37b425967c4dd80468248e50
Daniel K. O. (dkosmari) <none@none>
2024-10-12 11:13:58 -0300

    Removed `function_replacement_data_v2_t`.
    --HG--
    branch : remove-legacy-api-hg

--- include/function_patcher/fpatching_defines.h
+++ include/function_patcher/fpatching_defines.h
@@ -122,17 +122,6 @@ typedef enum FunctionPatcherTargetProcess {
 typedef uint32_t PatchedFunctionHandle;
 #define FUNCTION_REPLACEMENT_DATA_STRUCT_VERSION 0x00000003
 
-typedef struct function_replacement_data_v2_t {
-    uint32_t VERSION;
-    uint32_t physicalAddr;                       /* [needs to be filled]  */
-    uint32_t virtualAddr;                        /* [needs to be filled]  */
-    uint32_t replaceAddr;                        /* [needs to be filled] Address of our replacement function */
-    uint32_t *replaceCall;                       /* [needs to be filled] Address to access the real_function */
-    function_replacement_library_type_t library; /* [needs to be filled] rpl where the function we want to replace is. */
-    const char *function_name;                   /* [needs to be filled] name of the function we want to replace */
-    FunctionPatcherTargetProcess targetProcess;  /* [will be filled] */
-} function_replacement_data_v2_t;
-
 typedef struct function_replacement_data_t {
     uint32_t version;
     FunctionPatcherFunctionType type;
